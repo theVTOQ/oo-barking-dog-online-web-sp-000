00ea5319d571e3f7b7812251ae4eb99f39e21ed7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-02 15:09:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  #def initialize(name)
+    #@name = name
+    #@breed = breed
+  #end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
